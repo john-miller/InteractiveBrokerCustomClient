2f17448177c583de7dd8b87496d7400a045fe4fa
Jonathan Miller <jonathan@winepos.com>
2016-06-10 17:08:02 -0400

    Fixed bug where form would instantiate due to loop created from reference

--- src/main/java/com/miller/ibcc/gui/dashboard/FXDashboardForm.java
+++ src/main/java/com/miller/ibcc/gui/dashboard/FXDashboardForm.java
@@ -4,39 +4,38 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+import com.miller.ibcc.controller.ErrorController;
 import com.miller.ibcc.gui.FXApplicationFrame;
 import com.miller.ibcc.gui.injector.FXContentInjector;
-import com.sun.javafx.application.PlatformImpl;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public enum FXDashboardForm implements DashboardForm {
 	
 	INSTANCE;
 	
 	private Logger logger = Logger.getLogger(FXDashboardForm.class);
-	private AnchorPane pane;
+	private Pane pane;
 	
 	private FXDashboardForm() {
-		PlatformImpl.runAndWait(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					FXMLLoader loader = new FXMLLoader(FXApplicationFrame.class.getResource("/fxml/dashboard-form.fxml"));
-					loader.setController(FXDashboardForm.INSTANCE);
-					pane = loader.load();
-				} catch (IOException e) {
-					logger.error("Could not load FXML document", e);
-				} catch(IllegalStateException e) {
-					logger.error("Could not load FXML document", e);
-				}
-			} 
-		});
+		try {
+			logger.info("Creating FXDashboard UI");
+			FXMLLoader loader = new FXMLLoader(FXApplicationFrame.class.getResource("/fxml/dashboard-form.fxml"));
+			loader.setController(this);
+			pane = loader.load();
+		} catch (IOException e) {
+			logger.error("Could not load FXML document", e);
+			ErrorController.INSTANCE.displayError(new com.miller.ibcc.domain.Error("Dashboard Error", "Could not load the dashboard", ""));
+		} catch(IllegalStateException e) {
+			logger.error("Could not load FXML document", e);
+			ErrorController.INSTANCE.displayError(new com.miller.ibcc.domain.Error("Dashboard Error", "Could not load the dashboard", ""));
+		}
 	}
 
 	@Override
 	public void display() {
+		logger.info("Display dashboard form");
 		FXContentInjector.INSTANCE.injectContent(pane);
 	}
 
