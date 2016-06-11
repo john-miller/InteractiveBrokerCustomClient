d63ab255addf7b986c82ecf423099de019ff7b0a
Jonathan Miller <jonmiller44@gmail.com>
2016-06-11 11:42:36 -0400

    Added redirect to order controller

--- src/main/java/com/miller/ibcc/menu/orders/PlaceOrderMenuBarItem.java
+++ src/main/java/com/miller/ibcc/menu/orders/PlaceOrderMenuBarItem.java
@@ -2,6 +2,7 @@ package com.miller.ibcc.menu.orders;
 
 import org.apache.log4j.Logger;
 
+import com.miller.ibcc.controller.OrderController;
 import com.miller.ibcc.menu.MenuBarItem;
 
 public enum PlaceOrderMenuBarItem implements MenuBarItem {
@@ -23,6 +24,7 @@ public enum PlaceOrderMenuBarItem implements MenuBarItem {
 	@Override
 	public void onAction() {
 		logger.info("User placed order");
+		OrderController.INSTANCE.display();
 	}
 
 }
